2013f678a183c2620879e003dc3725570b4155c1
francescopanico <frpanico96@gmail.com>
2024-01-17 23:51:35 +0100

    [JSB] Review Service completion

--- src/main/java/com/net/pokemon/pokereview/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/net/pokemon/pokereview/exceptions/GlobalExceptionHandler.java
@@ -3,7 +3,6 @@ package com.net.pokemon.pokereview.exceptions;
 import java.util.Date;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -14,6 +13,18 @@ public class GlobalExceptionHandler {
 
   @ExceptionHandler(PokemonNotFoundException.class)
   public ResponseEntity<ErrorObject> handlePokemonNotFoundException(PokemonNotFoundException ex, WebRequest request) {
+    ErrorObject errorObject = new ErrorObject();
+
+    errorObject.setStatusCode(HttpStatus.NOT_FOUND.value());
+    errorObject.setMessage(ex.getMessage());
+    errorObject.setTimestamp(new Date());
+
+    return new ResponseEntity<ErrorObject>(errorObject, HttpStatus.NOT_FOUND);
+
+  }
+
+  @ExceptionHandler(ReviewNotFoundException.class)
+  public ResponseEntity<ErrorObject> handleReviewNotFoundException(PokemonNotFoundException ex, WebRequest request) {
     ErrorObject errorObject = new ErrorObject();
 
     errorObject.setStatusCode(HttpStatus.NOT_FOUND.value());
--- src/main/java/com/net/pokemon/pokereview/model/Review.java
+++ src/main/java/com/net/pokemon/pokereview/model/Review.java
@@ -10,10 +10,12 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
